64c163385390d05e2bad89a66beaabf5e601e00b
Sam Batschelet <sbatsche@redhat.com>
2019-06-24 08:56:45 -0400

    pkg/operator: add back resourcesynccontroller
    Signed-off-by: Sam Batschelet <sbatsche@redhat.com>

---
+++ pkg/operator/resourcesynccontroller/resourcesynccontroller.go
@@ -0,0 +1,42 @@
+package resourcesynccontroller
+
+import (
+	"net/http"
+
+	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
+
+	"github.com/openshift/library-go/pkg/operator/events"
+	"github.com/openshift/library-go/pkg/operator/resourcesynccontroller"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
+
+	"github.com/openshift/cluster-etcd-operator/pkg/operator/operatorclient"
+)
+
+func NewResourceSyncController(
+	operatorConfigClient v1helpers.OperatorClient,
+	kubeInformersForNamespaces v1helpers.KubeInformersForNamespaces,
+	configMapsGetter corev1client.ConfigMapsGetter,
+	secretsGetter corev1client.SecretsGetter,
+	eventRecorder events.Recorder) (*resourcesynccontroller.ResourceSyncController, http.Handler, error) {
+
+	resourceSyncController := resourcesynccontroller.NewResourceSyncController(
+		operatorConfigClient,
+		kubeInformersForNamespaces,
+		secretsGetter,
+		configMapsGetter,
+		eventRecorder,
+	)
+	if err := resourceSyncController.SyncConfigMap(
+		resourcesynccontroller.ResourceLocation{Namespace: operatorclient.TargetNamespace, Name: "etcd-serving-ca"},
+		resourcesynccontroller.ResourceLocation{Namespace: operatorclient.GlobalUserSpecifiedConfigNamespace, Name: "etcd-serving-ca"},
+	); err != nil {
+		return nil, nil, err
+	}
+	if err := resourceSyncController.SyncSecret(
+		resourcesynccontroller.ResourceLocation{Namespace: operatorclient.TargetNamespace, Name: "etcd-client"},
+		resourcesynccontroller.ResourceLocation{Namespace: operatorclient.GlobalUserSpecifiedConfigNamespace, Name: "etcd-client"},
+	); err != nil {
+		return nil, nil, err
+	}
+	return resourceSyncController, resourcesynccontroller.NewDebugHandler(resourceSyncController), nil
+}
--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -6,24 +6,27 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	configv1 "github.com/openshift/api/config/v1"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned"
 	configv1informers "github.com/openshift/client-go/config/informers/externalversions"
 	operatorversionedclient "github.com/openshift/client-go/operator/clientset/versioned"
 	operatorv1informers "github.com/openshift/client-go/operator/informers/externalversions"
+	"github.com/openshift/cluster-etcd-operator/pkg/operator/resourcesynccontroller"
 	"github.com/openshift/library-go/pkg/controller/controllercmd"
 	"github.com/openshift/library-go/pkg/operator/status"
+	"github.com/openshift/library-go/pkg/operator/v1helpers"
 
 	"github.com/openshift/cluster-etcd-operator/pkg/operator/operatorclient"
 )
 
 func RunOperator(ctx *controllercmd.ControllerContext) error {
 	// This kube client use protobuf, do not use it for CR
-	// kubeClient, err := kubernetes.NewForConfig(ctx.ProtoKubeConfig)
-	// if err != nil {
-	// 	return err
-	// }
+	kubeClient, err := kubernetes.NewForConfig(ctx.ProtoKubeConfig)
+	if err != nil {
+		return err
+	}
 	operatorConfigClient, err := operatorversionedclient.NewForConfig(ctx.KubeConfig)
 	if err != nil {
 		return err
@@ -36,11 +39,31 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 	if err != nil {
 		return err
 	}
+	kubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(kubeClient,
+		"",
+		operatorclient.GlobalUserSpecifiedConfigNamespace,
+		operatorclient.GlobalMachineSpecifiedConfigNamespace,
+		operatorclient.OperatorNamespace,
+		operatorclient.TargetNamespace,
+	)
+
 	operatorConfigInformers := operatorv1informers.NewSharedInformerFactory(operatorConfigClient, 10*time.Minute)
 	configInformers := configv1informers.NewSharedInformerFactory(configClient, 10*time.Minute)
+
 	operatorClient := &operatorclient.OperatorClient{
 		Informers: operatorConfigInformers,
 		Client:    operatorConfigClient.OperatorV1(),
+	}
+
+	resourceSyncController, debugHandler, err := resourcesynccontroller.NewResourceSyncController(
+		operatorClient,
+		kubeInformersForNamespaces,
+		v1helpers.CachedConfigMapGetter(kubeClient.CoreV1(), kubeInformersForNamespaces),
+		v1helpers.CachedSecretGetter(kubeClient.CoreV1(), kubeInformersForNamespaces),
+		ctx.EventRecorder,
+	)
+	if err != nil {
+		return err
 	}
 
 	// don't change any versions until we sync
@@ -55,9 +78,9 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 	versionRecorder.SetVersion("raw-internal", status.VersionForOperatorFromEnv())
 
 	clusterOperatorStatus := status.NewClusterOperatorStatusController(
-		"kube-apiserver",
+		"etcd",
 		[]configv1.ObjectReference{
-			{Group: "operator.openshift.io", Resource: "kubeapiservers", Name: "cluster"},
+			{Group: "operator.openshift.io", Resource: "etcds", Name: "cluster"},
 			{Resource: "namespaces", Name: operatorclient.GlobalUserSpecifiedConfigNamespace},
 			{Resource: "namespaces", Name: operatorclient.GlobalMachineSpecifiedConfigNamespace},
 			{Resource: "namespaces", Name: operatorclient.OperatorNamespace},
@@ -70,9 +93,14 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 		ctx.EventRecorder,
 	)
 
+	if ctx.Server != nil {
+		ctx.Server.Handler.NonGoRestfulMux.Handle("/debug/controllers/resourcesync", debugHandler)
+	}
+
 	operatorConfigInformers.Start(ctx.Done())
 
 	go clusterOperatorStatus.Run(1, ctx.Done())
+	go resourceSyncController.Run(1, ctx.Done())
 
 	<-ctx.Done()
 	return fmt.Errorf("stopped")
